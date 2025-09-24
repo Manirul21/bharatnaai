e9a461dd6a2249e729e27782b8445ea8e77e575e
mdman <mdmanirul6201@gmail.com>
2025-09-24 22:20:46 +0530

    intital git set-up

---
+++ src/main/java/com/bharatnaai/bharatnaaibackend/BharatnaaibackendApplication.java
@@ -0,0 +1,13 @@
+package com.bharatnaai.bharatnaaibackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BharatnaaibackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BharatnaaibackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bharatnaai/bharatnaaibackend/BharatnaaibackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bharatnaai.bharatnaaibackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BharatnaaibackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
